7e6ed2535bc46565d0a969d5b41d7698ce1e70e7
Lucas de Castro <lucasdec88@gmail.com>
2022-03-31 01:21:50 +0100

    Routes added, CSS in the routerLink, navigateByUrl to redirect to initial page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewTransferComponent } from './new-transfer/new-transfer.component';
+import { StatementComponent } from './statement/statement.component';
 
-const routes: Routes = [];
+//each object inside the routes variable, is a path
+export const routes: Routes = [
+  { path: '', redirectTo: 'statement', pathMatch: 'full' },//statement page is default
+  { path: 'statement', component: StatementComponent },
+  { path: 'new-transfer', component: NewTransferComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+
+}
--- src/app/new-transfer/new-transfer.component.ts
+++ src/app/new-transfer/new-transfer.component.ts
@@ -1,4 +1,5 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
 import { Transfer } from '../models/transfer.model';
 import { TransferService } from '../services/transfer.service';
 
@@ -16,14 +17,15 @@ export class NewTransferComponent {
   value: number;
   destination: number;
 
-  constructor(private service: TransferService) { }
+  constructor(private service: TransferService, private router: Router) { }
 
   transfer() {
     const valueToSend: Transfer = { value: this.value, destination: this.destination };
-    //this.afterSubmit.emit(valueToSend);
+    //this.afterSubmit.emit(valueToSend); NOW IS SENT DIRECTLY TO THE SERVICE
     this.service.add(valueToSend).subscribe(result => {
       console.log(result);
       this.clear();
+      this.router.navigateByUrl('statement');
     },
       (error) => console.error(error)
     );
